1ff515e9895e48abec70c8a20edfa20233f1a959
Iryna Shymbor <i.shymbor@gmail.com>
2020-06-04 20:09:28 +1000

    nicer data display for one past flight

--- fetch-a-launch.js
+++ fetch-a-launch.js
@@ -4,13 +4,25 @@ const fetch = require("node-fetch");
 
 const getLaunch = async(flightNum) => {
   try {
+    console.clear();
     const response = await fetch(`https://api.spacexdata.com/v3/launches/${flightNum}`);
     const data  = await response.json();
-    console.log(data);
+    const flight = {
+      flight_number: data.flight_number, 
+      mission_name: data.mission_name, 
+      launch_date: data.launch_date_local.slice(0, 10),
+      rocket_name: data.rocket.rocket_name,
+      rocket_type: data.rocket.rocket_type,
+      launch_success: data.launch_success,
+      details: data.details
+      };
+    console.log(flight);
   } catch(err) {
     console.log(err)
   }
 }
+
+console.clear();
 
 const options = [' Roadster distance from Mars', 'View future launches', 'View one past launch'];
 const index = readlineSync.keyInSelect(options, "What are you up to?") + 1;
