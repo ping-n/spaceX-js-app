9d34a8afbfbaac1190dba8a074c307f891399402
PING NGE <ping.nge@hotmail.com>
2020-06-05 00:30:54 +1000

    added app start entry point

---
+++ app.js
@@ -0,0 +1,31 @@
+const { Display } = require('./src/display');
+const getRoadsterData = require('./src/roadster');
+
+
+const app = async () => {
+
+  let start = true
+
+  while (start) {
+    Display.menu(user.name);
+    const choice = Display.getOption();
+
+    switch (choice) {
+      case 0:
+        start = false
+        break;
+      case 1: 
+        getRoadsterData();
+        break
+      case 2:
+        futureLaunches();
+        break;
+      case 3:
+        const flightNum = readlineSync.question('flight number: ')
+        getLaunch(flightNum);
+        break;
+      default:
+        console.log('Invalid Option');
+    }
+  }
+}
--- src/display.js
+++ src/display.js
@@ -1,16 +1,16 @@
-const readlineSync = require("readline-sync");
+const readlineSync = require('readline-sync');
 
 class Display {
-
   static menu() {
     console.log('Please select from the following options:');
-    console.log('1. Roadster distance from Mars');
+    console.log('0. Exit');
+    console.log('1. View Elon\'s roadster info');
     console.log('2. View future launches');
     console.log('3. View one past launches');
   }
 
   static getOption() {
-    return readlineSync.questionInt('> ')
+    return readlineSync.questionInt('> ');
   }
 }
 
