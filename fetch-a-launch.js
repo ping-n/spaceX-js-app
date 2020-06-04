9e96ef9cf7bf0d091fbe542ed2db6b9225ebcbb6
Iryna Shymbor <i.shymbor@gmail.com>
2020-06-04 20:50:17 +1000

    display upcoming flights

--- fetch-a-launch.js
+++ fetch-a-launch.js
@@ -1,22 +1,37 @@
 const readlineSync = require("readline-sync");
 const fetch = require("node-fetch");
 
+const flight = (data) => {
+  return {
+    flight_number: data.flight_number, 
+    mission_name: data.mission_name, 
+    launch_date: data.launch_date_local.slice(0, 10),
+    rocket_name: data.rocket.rocket_name,
+    rocket_type: data.rocket.rocket_type,
+    launch_success: data.launch_success,
+    details: data.details
+    };
+}
+
 
 const getLaunch = async(flightNum) => {
   try {
     console.clear();
     const response = await fetch(`https://api.spacexdata.com/v3/launches/${flightNum}`);
     const data  = await response.json();
-    const flight = {
-      flight_number: data.flight_number, 
-      mission_name: data.mission_name, 
-      launch_date: data.launch_date_local.slice(0, 10),
-      rocket_name: data.rocket.rocket_name,
-      rocket_type: data.rocket.rocket_type,
-      launch_success: data.launch_success,
-      details: data.details
-      };
-    console.log(flight);
+    console.log(flight(data));
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const futureLaunches = async() => {
+  try {
+    console.clear();
+    const response = await fetch(`https://api.spacexdata.com/v3/launches/upcoming`);
+    const data  = await response.json();
+    data.forEach((launch) => {
+    console.log(flight(launch))});
   } catch(err) {
     console.log(err)
   }
@@ -36,8 +51,7 @@ switch(index) {
     console.log('roadster');
     break;
   case 2:
-    // getLaunch();
-    console.log('future');
+    futureLaunches();
     break;
   case 3:
     const flightNum = readlineSync.question('flight number: ')
